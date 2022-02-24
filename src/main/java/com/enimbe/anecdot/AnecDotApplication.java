265a995e552b21d6240a7c6c047511e078f39629
303-10\megait <asdf@gmail.com>
2022-02-24 21:36:57 +0900

    [서예주] 0224 최초 생성

---
+++ src/main/java/com/enimbe/anecdot/AnecDotApplication.java
@@ -0,0 +1,13 @@
+package com.enimbe.anecdot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnecDotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnecDotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/enimbe/anecdot/user/UserController.java
@@ -0,0 +1,15 @@
+package com.enimbe.anecdot.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/signup")
+	public String signUp() {
+		return "user/signup";
+	}
+}
